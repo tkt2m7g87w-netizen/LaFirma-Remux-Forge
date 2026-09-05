cfa449709a0cc97d6318e13e7f254cbe189e4328
tkt2m7g87w-netizen <tkt2m7g87w@privaterelay.appleid.com>
2026-09-05 16:03:12 -0300

    Delete Converter_AUTO_DIRETO.bat

--- Converter_AUTO_DIRETO.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-title LaFirma - Motor no Console (v14.36) - DV 8.1 + Legenda PGS + Audio E-AC-3
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Converter_AUTO_DIRETO.ps1"
-echo.
-echo (fim - se a janela nao fechou sozinha, pode fechar manualmente)
-pause
